d1e2f4ae88fcaad4f02ec607ce9dc6be2699ff1c
caroreei <142321649+caroreei@users.noreply.github.com>
2024-06-04 12:17:43 +0300

    first commit

---
+++ view/footer.php
@@ -0,0 +1,9 @@
+	<footer class="main-footer">
+        <div class="pull-right hidden-xs">
+          <b>Version</b> 1.0.2
+        </div>
+        <strong>Copyright &copy; 2023-2030 <a href="">Regina Kagendo</a>.</strong> All rights reserved.
+     </footer>
+      
+  </body>
+</html>
---
+++ view/sidebar.php
@@ -0,0 +1,194 @@
+<!-- Left side column. contains the logo and sidebar -->
+  <aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+      <!-- Sidebar user panel -->
+<?php
+include_once('../controller/config.php');
+
+$index=$_SESSION["index_number"];
+
+$sql="SELECT * FROM admin WHERE index_number='$index'";
+$result=mysqli_query($conn,$sql);
+$row=mysqli_fetch_assoc($result);
+$name=$row['i_name'];
+$image=$row['image_name'];
+
+?>      
+      
+      <div class="user-panel">
+      	<div class="pull-left image">
+        	<img src="../<?php echo $image; ?>" class="img-circle" alt="User Image">
+        </div>
+        <div class="pull-left info">
+        	<p><?php echo $name; ?></p>
+            <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+        </div>
+      </div>
+      <!-- sidebar menu: : style can be found in sidebar.less -->
+      <ul class="sidebar-menu">
+        <li class="header">MAIN NAVIGATION</li>
+        <li class="active treeview">
+          <a href="dashboard.php">
+            <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+          </a>
+        </li>
+         <li>
+          <a href="admin_profile.php">
+            <i class="fa fa-flag"></i> <span>My Profile</span>
+          </a>
+        </li>
+        <li>
+          <a href="class_room.php">
+            <i class="fa fa-list-ul"></i> <span>Classroom</span>
+          </a>
+        </li>
+        <li>
+          <a href="grade.php">
+            <i class="fa fa-database"></i> <span>Form</span>
+          </a>
+        </li>
+        <li>
+          <a href="subject.php">
+            <i class="fa fa-book"></i> <span>Subject</span>
+          </a>
+        </li>
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-user" aria-hidden="true"></i>
+            <span>Teacher</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="teacher.php"><i class="fa fa-circle-o"></i> Add Teacher</a></li>
+            <li><a href="all_teacher.php"><i class="fa fa-circle-o"></i> All Teacher</a></li>
+          </ul>
+        </li>
+        <li>
+          <a href="subject_routing.php">
+            <i class="fa fa-th"></i> <span>Tuition Subject Routing</span>
+          </a>
+        </li>
+        <li>
+          <a href="timetable.php">
+            <i class="fa fa-calendar-plus-o"></i> <span>Timetable</span>
+          </a>
+        </li>
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-graduation-cap"></i>
+            <span>Student</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="student.php"><i class="fa fa-circle-o"></i> Add Student</a></li>
+            <li><a href="all_student.php"><i class="fa fa-circle-o"></i> All Student</a></li>
+            <li><a href="student_leave.php"><i class="fa fa-circle-o"></i> Leave Student</a></li>
+          </ul>
+        </li>
+        
+         <li class="treeview">
+          <a href="#">
+            <i class="fa fa-calendar-check-o"></i>
+            <span>Attendance</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="add_attendance.php"><i class="fa fa-circle-o"></i> Add Attendance</a></li>
+            <li><a href="student_attendance_history.php"><i class="fa fa-circle-o"></i>Student Attendance History </a></li>
+            <li><a href="teacher_attendance_history.php"><i class="fa fa-circle-o"></i> Teacher Attendance History</a></li>
+          </ul>
+        </li>
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-certificate"></i>
+            <span>Exam</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="exam.php"><i class="fa fa-circle-o"></i>Create Exam</a></li>
+            <li><a href="exam_timetable.php"><i class="fa fa-circle-o"></i> Exam Timetable</a></li>
+            <li><a href="student_exam_marks.php"><i class="fa fa-circle-o"></i>Student Exam Marks</a></li>
+            <li><a href="student_exam_marks_history.php"><i class="fa fa-circle-o"></i>Student Exam Marks History</a></li>
+          </ul>
+        </li>
+        <li>
+          <a href="petty_cash.php">
+            <i class="fa fa-yen"></i> <span>Petty Cash</span>
+          </a>
+        </li>
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-cash.php"></i>
+            <span>Accounting</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+          	<li><a href="../SchoolFeesMgSystem-PHP"></i> Accounting Module</a></li>
+           <li><a href="my_friends.php"><i class="fa fa-circle-o"></i></a>
+           
+          </ul>
+        </li>
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-calendar"></i>
+            <span>Events</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="my_events.php"><i class="fa fa-circle-o"></i> My Events</a></li>
+            <li><a href="all_events.php"><i class="fa fa-circle-o"></i> All Events</a></li>
+          </ul>
+        </li>
+        
+      </ul>
+
+      </li>
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-calendar"></i>
+            <span>Library</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="../libsystem"><i class="fa fa-circle-o"></i> My library</a></li>
+            <li><a href="../libsystem/admin
+            "><i class="fa fa-circle-o"></i>Librarian Panel </a></li>
+          </ul>
+        </li>
+        
+      </ul>
+
+      <li class="treeview">
+          <a href="#">
+            <i class="fa fa-calendar"></i>
+            <span>Online Exams</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="../ExamsCou/adminpanel"><i class="fa fa-circle-o"></i>Examiner</a></li>
+            <li><a href="../ExamsCou"><i class="fa fa-circle-o"></i>Examinee </a></li>
+          </ul>
+        </li>
+        
+      </ul>
+      
+    </section>
+    <!-- /.sidebar -->
+  </aside>
